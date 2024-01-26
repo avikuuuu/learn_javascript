d3847c310985e06870b7c5fa25041d78bc848b8e
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2024-01-26 22:55:32 +0530

    05 done

---
+++ 04-basic.js
@@ -0,0 +1,43 @@
+let open =32;
+const cv ='ab';
+var ad=23;
+ac=32;
+
+// cv=12;       not allow
+
+var ad =45;
+
+
+console.log(open);
+console.log(cv);
+console.log(ad);
+console.log(ac);
+
+// console.table([open,cv,ad,ac]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Variables and Data Types
+const = can't be re-assigned a value and can't be declared again  (block scope)
+let = can be re-assigned a value but can't be declared again (block scope)
+var = it can be re-assigned a value and it can also be declared again  (Global scope)
+
+=> Never use var in your code instead of let because of block and functional scope issues.
+# console.table([group of variables you want to show in tabular form])
+
+*/
